8d9e2cea77b9c33fe9b5ba9767660a49f6179b51
Eugene Kim <kehini20@gmail.com>
2026-07-01 22:18:34 +0900

    Update CLYM public namespace

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 android {
-    namespace = "com.starrydream.nanoclick"
+    namespace = "com.andantestudio.clym"
     compileSdk {
         version = release(36) {
             minorApiLevel = 1
